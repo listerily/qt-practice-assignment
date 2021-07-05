277df171d363149acffa02263a163941451f5aa1
listerily <listerily@outlook.com>
2021-07-05 15:16:11 +0800

    added a window

--- stardew-valley-lite/src/main.cpp
+++ stardew-valley-lite/src/main.cpp
@@ -1,11 +1,12 @@
 #include <QApplication>
 #include <QPushButton>
 
+#include "src/ui/gamemainwindow.h"
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    QPushButton button("Hello world!", nullptr);
-    button.resize(200, 100);
-    button.show();
+    GameMainWindow mainWindow;
+    mainWindow.show();
     return QApplication::exec();
 }
